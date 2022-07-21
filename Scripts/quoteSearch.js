21f934eae4a65c7349a50f72dba01a192d3397e0
YaaseenIbrahim <yaseenibrahimm12@gmail.com>
2022-07-21 09:51:48 +0500

    Added Altname searching -Golden quote when searched would appear always gold even without hover -Reused colors are set to variables in root -Text in each card is centered -The "fake" property added which removed quotation marks of the quote

--- Scripts/quoteSearch.js
+++ Scripts/quoteSearch.js
@@ -5,15 +5,22 @@ const SearchInput = document.querySelector("[data-search]")
 
 // Search
 let peeps = []
-SearchInput.addEventListener("input", e =>{
+SearchInput.addEventListener("input", e => {
     const value = e.target.value.toLowerCase();
-    
-    peeps.forEach(peep => {                         
-        let isVisible = 
-            peep.quote.toLowerCase().includes(value) || //OR
-            peep.person.toLowerCase().includes(value)||
-            peep.element.classList.contains("golden") && value == 'golden';
+
+    peeps.forEach(peep => {
         
+        const hasAlt = peep.altName != undefined && peep.altName.toLowerCase().includes(value)
+        const isGolden = peep.element.classList.contains("golden") && value == 'golden';
+        const isVisible =
+            peep.quote.toLowerCase().includes(value)||
+            peep.person.toLowerCase().includes(value)||
+            isGolden ||
+            hasAlt
+           
+        
+        peep.element.classList.toggle("alwaysGolden", isGolden) //Turns it gold always if you search Golden
+        peep.element.classList.toggle("center", isVisible)
         peep.element.classList.toggle("hide", !isVisible)
     })
 })
@@ -24,20 +31,22 @@ fetch("lob.json")
     .then((data) => {
         peeps = data.map(peep => {
             const Card = QuoteCardTemplate.content.cloneNode(true).children[0];
-            const Quote= Card.querySelector("[data-quote]");
-            const Person= Card.querySelector("[data-person]");
+            const Quote = Card.querySelector("[data-quote]");
+            const Person = Card.querySelector("[data-person]");
             Quote.textContent = peep.quote;
             Person.textContent = peep.person;
-            
-            if (peep.golden == true){
+
+            if (peep.golden == true) {
                 Card.classList.add("golden");
-            } 
+            }
             else if (peep.fake == true) {
                 Quote.classList.remove("actuallyQuote");
             }
+           
             CardContainer.append(Card);
-            return { quote: peep.quote , person: peep.person, element:Card}
+            return { quote: peep.quote, person: peep.person, altName: peep.altName, element: Card }
+
         })
-        
+
     });
 
