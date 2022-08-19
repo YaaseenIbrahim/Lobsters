351d2aca3b82604b345b59dc82d67ed926d4d5a2
CodeBlade <65892296+YaaseenIbrahim@users.noreply.github.com>
2022-08-19 14:58:04 +0500

    Darkmode & Settings (#2)
    - Dark mode added
    - Theme can be changed via theme changer button
    - Theme defaults to users default system theme
    - Settings UI added
    - Default theme can be changed via settings
    - Search animation trigger changed to onclick instead of onmouseover
    - Chance for quirky empty message increased from 10% to 20%
    - Can press Escape key to exit settings screen or press the Close X
    - Dhivehi font and searching possibility added
    - Default layout change be changed via settings
    - Reminder to refresh to see changes added

---
+++ Scripts/defaults.js
@@ -0,0 +1,97 @@
+const SettingsLayoutToggler = document.querySelector("#layout-checkbox")
+const SettingsThemeToggler = document.querySelector("#theme-checkbox");
+const Body = document.querySelector("body");
+const darkColors = {
+    originalQuoteBack: "#721732",
+    hoverQuoteBack: "#5c031c",
+    opacHov: "rgba(114, 23, 50, 0.6)",
+    accButtonBorder: "#5a0e24",
+    backgroundImg: "url(Pics/darkbg.jpg)",
+    blur: "rgba(180, 180, 180,0.5)",
+    qotdBlur: "rgba(180, 180, 180,0.4)",
+    backgroundcolor: "black",
+};
+const lightColors = {
+    originalQuoteBack: "#e7233a",
+    hoverQuoteBack: "#e2001a",
+    opacHov: "rgba(226, 0, 26, 0.6)",
+    accButtonBorder: "rgb(204, 30, 47)",
+    backgroundImg: "url(Pics/oilbg.png)",
+    blur: "rgba(255, 255, 255,0.5)",
+    qotdBlur: "rgba(255, 255, 255,0.3)",
+    backgroundcolor: "whitesmoke",
+};
+let theme = lightColors;
+//Default not set: autos to system theme
+if (localStorage.defaultTheme == undefined){
+    const isLight = window.matchMedia("(prefers-color-scheme: light)").matches || window.matchMedia("(prefers-color-scheme: no-preference)").matches;
+    themeSwitch(isLight, true)
+}
+//Following set default
+themeSwitch(localStorage.defaultTheme, "light")
+layoutSwitch(localStorage.defaultLayout, "grid")
+
+SettingsLayoutToggler.onclick = function () { layoutDefaults() }
+SettingsThemeToggler.onclick = function () { themeDefaults() }
+
+function toggleDefault(toggleButton, boolean, data1,  data2) {
+    if (data1 == data2) {
+        toggleButton.checked = boolean
+    }
+}
+function themeSwitch(data, option) {
+    
+    if (data == option){
+        ThemeButton.innerText = "light_mode";
+        ThemeButton.title = "Light Mode";
+        Body.style.color = "black";
+        EmptyImage.src = "Pics/lostbot.png";
+        theme = lightColors
+        toggleDefault(SettingsThemeToggler, true, data, localStorage.defaultTheme)
+    }
+    else{
+        ThemeButton.innerText = "nights_stay";
+        ThemeButton.title = "Dark Mode";
+        Body.style.color = "whitesmoke";
+        EmptyImage.src = "Pics/darklostbot.png";
+        theme = darkColors;
+        toggleDefault(SettingsThemeToggler, false, data, localStorage.defaultTheme)
+    }
+    Object.keys(theme).forEach((property) => {
+        const propString = "---" + property;
+        document.querySelector(":root").style.setProperty(propString, theme[property]);
+    });
+}
+function layoutSwitch(data, option) {
+    if (data == option){
+        LayoutButton.innerText = "grid_on"
+        AllCardsContainer.classList.add("gridLayout")
+        toggleDefault(SettingsLayoutToggler, true, data, localStorage.defaultLayout)
+    }
+    else{
+        LayoutButton.innerText = "table_rows"
+        AllCardsContainer.classList.remove("gridLayout")
+        toggleDefault(SettingsLayoutToggler, false, data, localStorage.defaultLayout)
+    }
+}
+
+function themeDefaults() {
+    if (SettingsThemeToggler.checked) {
+        localStorage.setItem("defaultTheme", "light")
+    } else {
+        localStorage.setItem("defaultTheme", "dark")
+    }
+    document.querySelector("#refreshReminder").children[0].classList.remove("imageHide")
+
+}
+
+function layoutDefaults() {
+    if (SettingsLayoutToggler.checked) {
+        localStorage.setItem("defaultLayout", "grid")
+    } else {
+        localStorage.setItem("defaultLayout", "flat")
+    }
+    document.querySelector("#refreshReminder").children[0].classList.remove("imageHide")
+
+}
+
--- Scripts/quoteLayout.js
+++ Scripts/quoteLayout.js
@@ -1,52 +1,62 @@
 //Small chance for quirky errors
-const errorMessages = ["Sadly momma ran out of roshi"]
-const errorText = document.querySelector("#emptyContainer").children[2]
+const errorMessages = ["Sadly momma ran out of roshi"];
+const errorText = document.querySelector("#emptyContainer").children[2];
 
-//Happens 10% of the time
-if (Math.random() > 0.9) {
-    errorText.innerText = errorMessages[Math.floor(Math.random() * errorMessages.length)]
+//Happens 20% of the time
+if (Math.random() > 0.8) {
+	errorText.innerText =
+		errorMessages[Math.floor(Math.random() * errorMessages.length)];
 }
 
 //Layout changer
-const LayoutButton = document.querySelector("#rows-icon")
-const ThemeButton = document.querySelector("#light-dark-mode")
-const QuoteContainer = document.querySelector(".quote-container")
-LayoutButton.onclick = function () { layoutSwitch() }
-ThemeButton.onclick = function () {themeSwitch()}
+const LayoutButton = document.querySelector("#rows-icon");
+const ThemeButton = document.querySelector("#light-dark-mode");
+const SettingsButton = document.querySelector("#settings-icon");
+const SettingsCloseBtn = document.querySelector("#close-btn");
+const QuoteContainer = document.querySelector(".quote-container");
+const EmptyImage = document.querySelector("#emptyImage");
 
-function layoutSwitch() {
-    if (LayoutButton.innerText == 'table_rows'){
-        LayoutButton.innerText = 'grid_on'
-        
-    }
-    else{
-        LayoutButton.innerText = 'table_rows'
-    }
+SettingsCloseBtn.onclick = function () { settingsPopUp(); };
+SettingsButton.onclick = function () { settingsPopUp(); };
 
-    AllCardsContainer.classList.toggle("gridLayout")  //AllCardsContainer is defined in quoteSearch.js
+ThemeButton.onclick = function () { themeSwitch(ThemeButton.innerText, "nights_stay") };
+LayoutButton.onclick = function () { layoutSwitch(LayoutButton.innerText, "table_rows") };
+
+//Opening and closing settings
+function settingsPopUp() {
+	document.querySelector("#popup-overlay").classList.toggle("imageHide");
+
+	SettingsCloseBtn.addEventListener("mousedown", function () {
+		SettingsCloseBtn.style.opacity = 0.75;
+	});
+	SettingsCloseBtn.addEventListener("mouseup", function () {
+		SettingsCloseBtn.style.opacity = 1;
+	});
+	document.addEventListener("keydown", function (event) {
+		if (event.key === "Escape") {
+			SettingsCloseBtn.style.opacity = 0.75;
+		}
+	});
+	document.addEventListener("keyup", function (event) {
+		if (event.key === "Escape") {
+			document.querySelector("#popup-container").classList.add("imageHide");
+		}
+		SettingsCloseBtn.style.opacity = 1;
+	});
+
 }
 
-function themeSwitch() {
-    if (ThemeButton.innerText == "light_mode") {
-        ThemeButton.innerText = "nights_stay"
-        ThemeButton.title = "Dark Mode"
-    }
-    else{
-        ThemeButton.innerText = "light_mode"
-        ThemeButton.title = "Light Mode"
 
-    }
-    
-}
 
-const SearchLabel = document.querySelector("#searchLabel")
-const SearchInputContainer = document.querySelector("#searchInput-container")
+// Search animation
+const SearchLabel = document.querySelector("#searchLabel");
+const SearchInputContainer = document.querySelector("#searchInput-container");
 
-SearchLabel.onmouseover = function() { highlighter() };
+SearchLabel.onclick = function () { highlighter(); };
 
 function highlighter() {
-    SearchLabel.classList.remove ("see-through")
-    SearchLabel.classList.remove ("OG-searchLabel")
-    SearchLabel.classList.add ("hover-searchLabel")
-    SearchInputContainer.classList.add("hover-searchInput") 
+	SearchLabel.classList.remove("see-through");
+	SearchLabel.classList.remove("OG-searchLabel");
+	SearchLabel.classList.add("hover-searchLabel");
+	SearchInputContainer.classList.add("hover-searchInput");
 }
